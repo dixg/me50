2364bb600505b3badd31c2c5af5716d26f36e718
dixg <dixg@users.noreply.github.com>
2020-01-08 09:16:49 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -8,7 +8,7 @@ int main ()
     printf("Height: ");
     scanf("%d",&height);
     }
-    while (height<0 || height>8);  
+    while (height<1 || height>8);  
     for(int i=1; i<=height; i++)
       {
             for( int j=1; j<=height-i; j++)
