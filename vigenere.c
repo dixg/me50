f7d32af97d15ae8d37aa15a70b2c4984c8a2045e
dixg <dixg@users.noreply.github.com>
2020-01-28 01:36:35 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- vigenere.c
+++ vigenere.c
@@ -1,25 +1,49 @@
 #include <cs50.h>
 #include <stdio.h>
-int for_Key(char letter);
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int main(void)
-{ 
-    char alpha;
-    printf("Enter the aplhabet to convert: ");
-    scanf("%c", &alpha);
-    for_Key(alpha);
+int shift(char c);
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./vigenere keyword\n");
+        return 1;
+    }
+    
+    //int k = atoi(argv[1]);
+    if (isdigit(atoi(argv[1]))) 
+    {
+        printf("Usage: ./vigenere keyword\n");
+        return 1;
+    }
+    
+    int key = shift(argv[1][0]);
+    printf("%i\n", key);
+    
+    //string s = get_string("plaintext:  ");
+    //int n = strlen(s);
+    //printf("ciphertext: "); 
+
 }
     
-int for_Key(char letter)
-{     
-    int char_asci=c;       
-    if(char_asci>=97 && char_asci<=122)
+int shift(char c)
+{ 
+    int char_asci = c; 
+    
+    if (char_asci >= 97 && char_asci <= 122)
     {
-        int key=char_asci-97;
+        int key = char_asci - 97;
         return key;
     }
-     else 
-        {
-           return -1;
-        }
-     }  
+    else 
+    {
+        return -1;
+    }
+}
+     
+  
+
