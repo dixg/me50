8ba0b42e8ea109d893675ac3272921ac44625709
dixg <dixg@users.noreply.github.com>
2020-06-12 21:08:39 +0000

    automated commit by submit50

--- databases/print_csv.py
+++
@@ -1,5 +0,0 @@
-import csv
-with open('small.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
